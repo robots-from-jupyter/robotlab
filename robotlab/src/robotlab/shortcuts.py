ae68b8d56a6450f7ea2dfcea8fa6cddebc14e751
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-01-13 11:27:56 -0500

    cleaning

--- robotlab/src/robotlab/shortcuts.py
+++ robotlab/src/robotlab/shortcuts.py
@@ -5,10 +5,10 @@ from . import HERE, ICON_EXT
 
 def make_shortcuts():
     script = str(HERE / "launch.py")
-    print("Making shortcut to", script)
     pyshortcuts.make_shortcut(
         script=script,
         name="RobotLab",
+        folder="RobotLab",
         terminal=True,
         description="Launch RobotLab in your $HOME",
         icon=str(HERE / "icons" / f"lab.{ICON_EXT}")
